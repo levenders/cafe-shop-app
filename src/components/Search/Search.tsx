aa3d434aa93f53ab804bdef66ab88273d056c7b9
Lev Enders <leveders@mail.ru>
2024-05-20 23:25:36 +0300

    feat: add Search component

---
+++ src/components/Search/Search.tsx
@@ -0,0 +1,37 @@
+import { DetailedHTMLProps, InputHTMLAttributes, forwardRef } from 'react'
+import cn from 'classnames'
+import Image from 'next/image'
+
+import styles from './Search.module.css'
+
+interface ISearchProps
+  extends DetailedHTMLProps<
+    InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement
+  > {
+  isValid?: boolean
+}
+
+export const Search = forwardRef<HTMLInputElement, ISearchProps>(function Input(
+  { className, isValid, ...props },
+  ref
+) {
+  return (
+    <div className={styles.wrapper}>
+      <input
+        className={cn(className, styles.input, {
+          [styles.valid]: isValid,
+        })}
+        ref={ref}
+        {...props}
+      />
+      <Image
+        className={styles.icon}
+        src="/search.svg"
+        width="20"
+        height="20"
+        alt="search-logo"
+      />
+    </div>
+  )
+})
