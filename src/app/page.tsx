94228c115c8c41ed23852f362a181e53b82d81e6
Lev Enders <leveders@mail.ru>
2024-05-20 22:43:53 +0300

    feat: add cart page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { Input } from '@/components/Input'
 export default function Home() {
   return (
     <>
+      <h1>Меню</h1>
       <Button>Button</Button>
       <Button appearance="big">Button</Button>
       <Input placeholder="Email" />
