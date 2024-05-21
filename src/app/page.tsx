97bd64059d880194093d1f705b6da139560282bd
Lev Enders <leveders@mail.ru>
2024-05-21 23:26:03 +0300

    feat: add ProductCard component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,36 @@
-import { Headling } from '@/components/Headling'
-import { Search } from '@/components/Search'
+import { Headling, ProductCard, Search } from '@/components'
 
 import styles from './Menu.module.css'
 
 export default function Menu() {
+  const getMenu = async () => {
+    const res = await fetch('')
+  }
+
   return (
-    <div className={styles.head}>
-      <Headling>Меню</Headling>
-      <Search placeholder="Введите блюдо или состав"></Search>
-    </div>
+    <>
+      <div className={styles.head}>
+        <Headling>Меню</Headling>
+        <Search placeholder="Введите блюдо или состав"></Search>
+      </div>
+      <div>
+        <ProductCard
+          id={1}
+          title="Наслаждение"
+          description="Салями, руккола, помидоры, оливки"
+          rating={4.5}
+          price={300}
+          image="/product-demo.png"
+        />
+        <ProductCard
+          id={2}
+          title="Наслаждение"
+          description="Салями, руккола, помидоры, оливки"
+          rating={4.5}
+          price={300}
+          image="/product-demo.png"
+        />
+      </div>
+    </>
   )
 }
---
+++ src/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,50 @@
+import { FC } from 'react'
+import Image from 'next/image'
+import styles from './ProductCard.module.css'
+import { priceRu } from '@/helpers'
+import Link from 'next/link'
+
+interface IProductCardProps {
+  id: number
+  title: string
+  description: string
+  image: string
+  price: number
+  rating: number
+}
+
+export const ProductCard: FC<IProductCardProps> = ({
+  id,
+  title,
+  description,
+  image,
+  price,
+  rating,
+}) => {
+  return (
+    <Link href={`/product/${id}`} className={styles.card}>
+      <div
+        className={styles.header}
+        style={{ backgroundImage: `url(${image})` }}
+      >
+        <div className={styles.price}>{priceRu(price)}</div>
+        <button className={styles['add-to-cart']}>
+          <Image
+            src="/plus.svg"
+            width="25"
+            height="25"
+            alt="add-to-cart-button"
+          />
+        </button>
+        <div className={styles.rating}>
+          {rating}
+          <Image src="/star.svg" width="14" height="14" alt="rating-star" />
+        </div>
+      </div>
+      <div className={styles.footer}>
+        <div className={styles.title}>{title}</div>
+        <div className={styles.description}>{description}</div>
+      </div>
+    </Link>
+  )
+}
